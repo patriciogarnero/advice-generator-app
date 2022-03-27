5faadee4a24f7037d74debf43ca7a2e0a3534c8d
patriciogarnero <80570463+patriciogarnero@users.noreply.github.com>
2022-03-27 17:07:12 +0200

    finished mobile layout

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const adviceId = document.getElementById("id");
 const advice = document.getElementById("advice");
+const dice = document.querySelector(".dice");
+
+dice.addEventListener("click", () => {
+  getData();
+});
 
 const getData = async () => {
   const response = await fetch("https://api.adviceslip.com/advice");
@@ -7,5 +12,3 @@ const getData = async () => {
   adviceId.innerHTML = "ADVICE " + data.slip.id;
   advice.innerHTML = data.slip.advice;
 };
-
-getData();
